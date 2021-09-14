eeb07c7684411645621b9dd45e8ab30fba4d976f
yuta_begginer <sy03141995139@yahoo.co.jp>
2021-09-14 10:46:49 +0900

    finish 2-17

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,6 +3,7 @@ class Todo < ApplicationRecord
   has_many :todo_tags
   has_many :tags, through: :todo_tags
   accepts_nested_attributes_for :todo_tags
+  
   acts_as_list scope: [:goal_id]
 
   validates :position, numericality: { only_integer: true, allow_nil: true }
